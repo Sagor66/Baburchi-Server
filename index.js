2db0b5a7ab2de2348e8c886eedb0d8565f2027b5
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-05-03 02:31:24 +0600

    added bio in chefs.json

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express();
 const cors = require("cors");
 const port = process.env.Port || 5000;
 
+const chefs = require('./data/chefs.json')
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chef is cooking");
 });
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
+})
 
 
 
